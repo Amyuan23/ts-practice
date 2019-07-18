57f9fe1c8a1ab96e1a4b009af6c3f20662138893
yuan xue <amyuan23@gmail.com>
2019-07-18 10:31:34 +0800

    练习

--- 1.ts
+++ 1.ts
@@ -1,19 +1,6 @@
-class Student {
-  fullName: string
-  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-    this.fullName = firstName + ' ' + middleInitial + ' ' + lastName
-  }
+#!/usr/bin/env ts-node
+function add(a: number, b: number): void {
+  console.log(a + b)
 }
 
-interface Person {
-  firstName: string
-  lastName: string
-}
-
-function greeter(person: Person) {
-  return 'Hello, ' + person.firstName + ' ' + person.lastName
-}
-
-let user = new Student('Jane', 'M.', 'User')
-
-console.log(greeter(user))
+add(2, 3)
